9a7b5dfb68bbfbfba5cc4be38316c76fc3740804
Dilki Poornima <dilkipw1218@gmail.com>
2022-04-20 12:27:13 +0530

    Workout Feature List View Done

--- Project/frontend/src/pages/workout/WorkoutList.js
+++ Project/frontend/src/pages/workout/WorkoutList.js
@@ -1,36 +1,101 @@
-import React, { useState, useEffect } from "react";
-import WorkoutAPI from "../../contexts/api/WorkoutAPI";
+/* eslint-disable indent */
+import React, { useContext, useState } from "react";
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme, Modal } from "@mantine/core";
+import WorkoutContext from "../../contexts/WorkoutContext";
 
-const WorkoutList = () => {
-	const [workouts, setWorkouts] = useState([]);
+import { Edit, Trash } from "tabler-icons-react";
+import EditWorkout from "./EditWorkout";
+import View from "./View";
 
-	//Get all workouts
-	useEffect(() => {
-		WorkoutAPI.getWorkoutData().then((response) => {
-			setWorkouts(response.data);
-		});
-	}, []);
+function WorkoutList() {
+	const { workouts, confirmDelete, editOpened, setEditOpened, setWorkout } = useContext(WorkoutContext);
+	const theme = useMantineTheme();
+
+	const secondaryColor = theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
+
+	// View Modal
+	const [opened, setOpened] = useState(false);
 
 	return (
-		<div>
-			<h2>WorkoutList</h2>
+		<>
+			{/* Edit Modal */}
+			<Modal opened={editOpened} onClose={() => setEditOpened(false)} title="Edit Workout ">
+				<EditWorkout />
+			</Modal>
 
-			{workouts.map((workout) => (
-				<div key={workout._id}>
-					<p> Workout Name :{workout.workout_name} </p>
-					<p> Workout Category :{workout.workout_category} </p>
-					<p> Muscle group :{workout.muscle_group} </p>
-					<p> Starting position img :{workout.starting_position_img} </p>
-					<p> Mid position img :{workout.mid_position_img} </p>
-					<p> Instructions :{workout.instructions} </p>
-					<p> Action :{workout.action} </p>
-					<p> Tips :{workout.tips} </p>
-					<p> Created At :{workout.createdAt} </p>
-					<p> viewCount :{workout.viewCount} </p>
+			{/* View Modal */}
+			<Modal opened={opened} onClose={() => setOpened(false)} title="Workout Details" overflow="inside" size="5">
+				<View />
+			</Modal>
+
+			{workouts.map((item) => (
+				<div key={item._id} style={{ width: 340, margin: "auto" }}>
+					<Card shadow="sm" p="lg">
+						<Card.Section>
+							<Image src={item.starting_position_img} height={160} alt="Norway" />
+						</Card.Section>
+
+						<Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+							<Text weight={500}>{item.workout_name}</Text>
+						</Group>
+
+						<Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+							{item.instructions}
+						</Text>
+						<Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+							{item.tips}
+						</Text>
+
+						<hr style={{ border: `1px solid ${secondaryColor}` }} />
+
+						<Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+							workout_category: {item.workout_category}
+						</Text>
+						<Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+							action: {item.action}
+						</Text>
+
+						{/* On click open modal */}
+						<Button
+							variant="light"
+							color="blue"
+							fullWidth
+							style={{ marginTop: 14 }}
+							onClick={() => {
+								setWorkout(item);
+								setOpened(true);
+							}}
+						>
+							View
+						</Button>
+
+						{localStorage.getItem("permissionLevel") === "ADMIN" ||
+						localStorage.getItem("permissionLevel") === "TRAINER" ? (
+							<Group position="apart" mt="md" spacing="md">
+								<Button
+									onClick={() => {
+										setWorkout(item);
+										setEditOpened(true);
+									}}
+									variant="light"
+									color="blue"
+									compact
+									leftIcon={<Edit size={16} />}
+								>
+									Edit
+								</Button>
+								<Button onClick={() => confirmDelete(item._id)} color="red" compact leftIcon={<Trash size={16} />}>
+									Delete
+								</Button>
+							</Group>
+						) : (
+							<p></p>
+						)}
+					</Card>
 				</div>
 			))}
-		</div>
+		</>
 	);
-};
+}
 
 export default WorkoutList;
