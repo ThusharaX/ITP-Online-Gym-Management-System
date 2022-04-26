543d009d0cb6d5e471880b204938bad7a3afa5e6
Nikethani Gangoda <wathsalagangoda@gmail.com>
2022-04-26 23:17:18 +0530

    Implenet Workout Schedule Request Retrieve

--- Project/frontend/src/routes/app-routes.js
+++ Project/frontend/src/routes/app-routes.js
@@ -29,6 +29,7 @@ import {
 	AdminDashboard,
 	AdminProfile,
 	CommonLogin,
+	WorkoutScRList,
 } from "../pages";
 
 // Error pages
@@ -91,7 +92,9 @@ const AppRoutes = () => {
 
 					<Route exact path="/workoutProgram" element={<WorkoutProgram />} />
 
+					{/*Workout Schedule Request */}
 					<Route path="/workoutscr" element={<WorkoutScR />} />
+					<Route path="/workoutscrlist" element={<WorkoutScRList />} />
 
 					<Route exact path="/login" element={<CheckLoginStatus />}>
 						<Route exact path="/login" element={<CommonLogin />} />
