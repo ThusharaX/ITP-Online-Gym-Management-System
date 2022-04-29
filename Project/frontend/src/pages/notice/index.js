348fa7b94fceb6a2d263bcfe5e47867349b33e38
Tharushi Perera <82897351+hvneypop@users.noreply.github.com>
2022-04-29 08:47:03 +0530

    feature-notice-updated

--- Project/frontend/src/pages/notice/index.js
+++ Project/frontend/src/pages/notice/index.js
@@ -6,18 +6,25 @@ import AddNotice from "./AddNotice";
 // Notice Provider
 import { NoticeProvider } from "../../contexts/NoticeContext";
 import NoticeList from "./NoticeList";
+import "./index.css";
 
 const Notice = () => {
 	return (
-		<div>
-			<h1 style={{ color: "red" }}>Notices</h1>
+		<>
+			<div className="notice-form-container">
+				<h1 style={{ color: "red" }}>Notices</h1>
 
-			<NoticeProvider>
-				<NoticeList />
-				{/* Add new Notice */}
-				<AddNotice />
-			</NoticeProvider>
-		</div>
+				<NoticeProvider>
+					{/* Add new Notice */}
+					<AddNotice />
+				</NoticeProvider>
+			</div>
+			<div>
+				<NoticeProvider>
+					<NoticeList />
+				</NoticeProvider>
+			</div>
+		</>
 	);
 };
 
