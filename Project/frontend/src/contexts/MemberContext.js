f826abc8b522c825ec65cdaa9215df620460b023
Tharushi Perera <82897351+hvneypop@users.noreply.github.com>
2022-05-16 12:04:35 +0530

    member-feature-updated

--- Project/frontend/src/contexts/MemberContext.js
+++ Project/frontend/src/contexts/MemberContext.js
@@ -1,9 +1,56 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+import { useForm } from "@mantine/form";
 
 const MemberContext = createContext();
+import UserAPi from "./api/UserAPI";
 
 export function MemberProvider({ children }) {
-	return <MemberContext.Provider value={{}}>{children}</MemberContext.Provider>;
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [message, setMessage] = useState(null);
+
+	const form = useForm({
+		initialValues: {
+			username: "",
+			password: "",
+		},
+
+		validate: {
+			password: (value) => (value.length >= 4 ? null : "Password must be at least 4 characters"),
+		},
+	});
+
+	const login = (values) => {
+		setIsLoading(true);
+		UserAPi.login(values)
+			.then((response) => {
+				if (response.data.permissionLevel !== "MEMBER") {
+					setIsLoading(false);
+					return alert("You are not a Member");
+				} else {
+					localStorage.setItem("uID", response.data._id);
+					localStorage.setItem("username", response.data.username);
+					localStorage.setItem("authToken", response.data.token);
+					localStorage.setItem("permissionLevel", response.data.permissionLevel);
+					window.location.href = "/member";
+					setIsLoggedIn(true);
+					setIsLoading(false);
+				}
+			})
+			.catch((err) => {
+				setMessage(err.response.data.details.message);
+				setIsLoading(false);
+			});
+	};
+
+	return (
+		<MemberContext.Provider
+			value={{ login, isLoading, setIsLoading, isLoggedIn, setIsLoggedIn, form, message, setMessage }}
+		>
+			{children}
+		</MemberContext.Provider>
+	);
 }
 
 export default MemberContext;
