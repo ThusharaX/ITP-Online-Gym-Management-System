762e6bb3c81228cc4cbd3a40ea6b0ce2211c8c61
Gaween kanishka <gaweenkanishka@gmail.com>
2022-05-25 21:31:56 +0530

    add some css and componenmts to question section to Inprove UX

--- Project/frontend/src/pages/question/index.js
+++ Project/frontend/src/pages/question/index.js
@@ -3,33 +3,35 @@ import React from "react";
 // Page components
 import QuestionList from "./QuestionList";
 import AddQuestion from "./AddQuestion";
-import FAQ from "./FAQ";
 import FAQQ from "./FAQQ";
-
+import QuestionTableScrollArea from "./QuestionTableScrollArea";
 // QuestionProvider
 import { QuestionProvider } from "../../contexts/QuestionContext";
+import HeroImageBackground from "./Banner";
 
 const Question = () => {
 	return (
 		<div
 			style={{
 				backgroundImage:
-					"url(https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870)",
+					"url(https://images.unsplash.com/photo-1608311397926-af25e955eb4a?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327)",
 				backgroundSize: "1300px",
 				height: "100%",
 				backgroundPosition: "center",
-				backgroundRepeat: "no-repeat",
 
 				// height: "100vh",
 			}}
 		>
-			<FAQQ />
 			<QuestionProvider>
+				<HeroImageBackground />
 				{/* Question list */}
-				<QuestionList />
 
+				<QuestionList />
+				<FAQQ />
 				{/* Add new Question */}
 				<AddQuestion />
+
+				<QuestionTableScrollArea />
 			</QuestionProvider>
 			{/* <FAQ /> */}
 		</div>
